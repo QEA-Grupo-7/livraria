c117be902fc83c67e9d6d928d6fa995864607007
Henrique Bustillos <henrique.ssbl@hotmail.com>
2022-08-06 11:20:41 -0300

    feature: refatoração e buscarAutorPeloNome
    Co-authored-by: Higea Souza <higeasouza@users.noreply.github.com>

--- src/Autor.java
+++ src/Autor.java
@@ -1,4 +1,8 @@
 public class Autor {
 
   private String nome;
+
+  public String getNome() {
+    return this.nome;
+  }
 }
--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,6 @@ import java.util.Scanner;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Stream;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -36,49 +35,11 @@ public class Main {
         String telefone = scanner.nextLine();
 
         System.out.println("Cadastrando usuário...");
+        System.out.println("Cadastro realizado com sucesso!");
 
         Usuario usuario = new Usuario(nome, cpf, telefone);
 
-        System.out.println("Escolha uma opção: ");
-        System.out.println("1 - Verificar se obra existe no acervo");
-        System.out.println("0 - Sair");
-        String opcao = scanner.nextLine();
-
-        switch (opcao) {
-            case "0": {
-                System.out.println("Saindo...");
-                break;
-            }
-            case "1": {
-                System.out.println("Digite o nome da obra: ");
-                String busca = scanner.nextLine();
-
-                System.out.println("Buscando livro com nome '" + busca + "'...");
-
-                List<Livro> livros = getLivros();
-
-                Livro livroEncontrado = null;
-
-                for (Livro livro : livros) {
-                    if (livro.getTitulo().equals(busca)) {
-                        livroEncontrado = livro;
-                        break;
-                    }
-                }
-
-                if (livroEncontrado != null) {
-                    System.out.println("Livro encontrado no acervo!\n");
-                    System.out.println("Título: " + livroEncontrado.getTitulo());
-                    System.out.println("Autor: " + livroEncontrado.getAutor());
-                    System.out.println("Editora: " + livroEncontrado.getEditora());
-                    System.out.println("Ano de publicação: " + livroEncontrado.getAno());
-                    System.out.println("ISBN: " + livroEncontrado.getISBN());
-                    System.out.println("");
-                } else {
-                    System.out.println("Livro não encontrado no acervo!\n");
-                }
-            }
-        }
+        menu(scanner);
 
         // Exemplar exemplar = new Exemplar(
         // "1",
@@ -97,6 +58,85 @@ public class Main {
         // emprestimo.setDataDeEntregaReal(dataEntregaFigurativa);
 
         // verificarEntrega(emprestimo);
+    }
+
+    private static void menu(Scanner scanner) throws IOException {
+        System.out.println("Escolha uma opção: ");
+        System.out.println("0 - Sair");
+        System.out.println("1 - Verificar se obra existe no acervo");
+        System.out.println("2 - Buscar autor pelo nome");
+        String opcao = scanner.nextLine();
+
+        switch (opcao) {
+            case "0": {
+                System.out.println("Saindo...");
+                break;
+            }
+            case "1": {
+                buscarObraNoAcervo(scanner);
+                break;
+            }
+            case "2": {
+                buscarAutorPeloNome(scanner);
+                break;
+            }
+        }
+    }
+
+    private static void buscarObraNoAcervo(Scanner scanner) throws IOException {
+        System.out.println("Digite o nome da obra: ");
+        String busca = scanner.nextLine();
+
+        System.out.println("Buscando livro com nome '" + busca + "'...");
+
+        List<Livro> livros = getLivros();
+
+        Livro livroEncontrado = null;
+
+        for (Livro livro : livros) {
+            if (livro.getTitulo().equals(busca)) {
+                livroEncontrado = livro;
+                break;
+            }
+        }
+
+        if (livroEncontrado != null) {
+            System.out.println("Livro encontrado no acervo!\n");
+            System.out.println("Título: " + livroEncontrado.getTitulo());
+            System.out.println("Autor: " + livroEncontrado.getAutor());
+            System.out.println("Editora: " + livroEncontrado.getEditora());
+            System.out.println("Ano de publicação: " + livroEncontrado.getAno());
+            System.out.println("ISBN: " + livroEncontrado.getISBN());
+            System.out.println("");
+        } else {
+            System.out.println("Livro não encontrado no acervo!\n");
+        }
+    }
+
+    private static void buscarAutorPeloNome(Scanner scanner) throws IOException {
+        System.out.println("Digite o nome do autor: ");
+        String busca = scanner.nextLine();
+
+        System.out.println("Buscando autor com nome '" + busca + "'...");
+
+        List<Autor> autores = getAutores();
+
+        Autor autorEncontrado = null;
+
+        for (Autor autor : autores) {
+            if (autor.getNome().equals(busca)) {
+                autorEncontrado = autor;
+                break;
+            }
+        }
+
+        if (autorEncontrado != null) {
+            System.out.println("Autor encontrado!\n");
+            System.out.println("Nome: " + autorEncontrado.getNome());
+            System.out.println("");
+        } else {
+            System.out.println("Autor não encontrado!\n");
+        }
     }
 
     public static Emprestimo criarEmprestimo(Exemplar exemplar, Usuario usuario) {
@@ -177,7 +217,7 @@ public class Main {
     private static List<Autor> getAutores() throws IOException {
         Gson gson = new Gson();
 
-        Reader reader = Files.newBufferedReader(Paths.get(PATH + "author.json"), StandardCharsets.UTF_8);
+        Reader reader = Files.newBufferedReader(Paths.get(PATH + "authors.json"), StandardCharsets.UTF_8);
 
         Type autoresType = new TypeToken<ArrayList<Autor>>() {
         }.getType();
